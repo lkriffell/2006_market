f7340ab3e7275d2033a63cec9e75cf7c1aacadec
Logan Riffell <lkriffell@gmail.com>
2020-07-29 09:01:25 -0600

    Add test_it_exits and test_it_has_attributes for item class

---
+++ test/item_test.rb
@@ -0,0 +1,18 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "./lib/item"
+
+class ItemTest < Minitest::Test
+
+  def test_it_exists
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    assert_instance_of Item, item1
+  end
+
+  def test_it_has_attributes
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    assert_equal "Peach", item1.name
+    assert_equal "$0.75", item1.price
+  end
+
+end
