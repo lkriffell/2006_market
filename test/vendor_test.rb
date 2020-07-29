f533d54248bdf29327087331f8bac9e8456c3904
Logan Riffell <lkriffell@gmail.com>
2020-07-29 09:06:17 -0600

    Add test_it_has_attributes for vendor class

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -10,7 +10,11 @@ class VendorTest < Minitest::Test
     assert_instance_of Vendor, vendor
   end
 
-  #def test_it_has_attributes
-  #end
+  def test_it_has_attributes
+    vendor = Vendor.new("Rocky Mountain Fresh")
+
+    assert_equal "Rocky Mountain Fresh", vendor.name
+    assert_equal ({}), vendor.inventory
+  end
 
 end
