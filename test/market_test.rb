61a932087f1491b3195954e9abbc8103b599a940
Logan Riffell <lkriffell@gmail.com>
2020-07-29 09:34:32 -0600

    Add test_it_has_attributes for market class

--- test/market_test.rb
+++ test/market_test.rb
@@ -11,7 +11,12 @@ class MarketTest < Minitest::Test
     assert_instance_of Market, market
   end
 
-  #def test_it_has_attributes
-  #end
+  def test_it_has_attributes
+    market = Market.new("South Pearl Street Farmers Market")
+    
+    assert_equal "South Pearl Street Farmers Market", market.name
+    assert_equal [], market.vendors
+
+  end
 
 end
