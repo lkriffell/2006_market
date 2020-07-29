d69e699126ea024601b89f288f171fa0d05e49f5
Logan Riffell <lkriffell@gmail.com>
2020-07-29 09:36:28 -0600

    Add test_add_vendor and corresponding method for market class

--- test/market_test.rb
+++ test/market_test.rb
@@ -13,10 +13,19 @@ class MarketTest < Minitest::Test
 
   def test_it_has_attributes
     market = Market.new("South Pearl Street Farmers Market")
-    
+
     assert_equal "South Pearl Street Farmers Market", market.name
     assert_equal [], market.vendors
+  end
 
+  def test_add_vendor
+    market = Market.new("South Pearl Street Farmers Market")
+    vendor1 = Vendor.new("Rocky Mountain Fresh")
+    vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    market.add_vendor(vendor1)
+    market.add_vendor(vendor2)
+
+    assert_equal [vendor1, vendor2], market.vendors
   end
 
 end
